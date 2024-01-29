8e8ba6c0a296297a5e0aa8b97aff402a57533f7a
haris ahmad <harismushtaq59@gmail.com>
2024-01-29 11:07:51 +0530

    website

---
+++ script.js
@@ -0,0 +1,46 @@
+const toggleButton = document.getElementById("nav-toggle");
+const navLinks = document.getElementById("nav-links");
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// services box
+const boxViews = document.querySelectorAll(".services-box"),
+  boxBtns = document.querySelectorAll(".services-button"),
+  boxCloses = document.querySelectorAll(".services-box-close");
+
+let box = function (boxClick) {
+  boxViews[boxClick].classList.add("active-box");
+};
+
+boxBtns.forEach((boxBtn, i) => {
+  boxBtn.addEventListener("click", () => {
+    box(i);
+  });
+});
+
+boxCloses.forEach((boxClose) => {
+  boxClose.addEventListener("click", () => {
+    boxViews.forEach((boxView) => {
+      boxView.classList.remove("active-box");
+    });
+  });
+});
+
+// change bg header
+function scrollHeader() {
+  const nav = document.getElementById("home");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+// scroll to top
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
